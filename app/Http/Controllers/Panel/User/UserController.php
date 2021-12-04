76669e670ff945f2668cf8b81dec891bb0c6ced9
amir-ys <amir.you74@gmail.com>
2021-12-04 23:21:32 +0330

    using Datatable for users index table

--- app/Http/Controllers/Panel/User/UserController.php
+++ app/Http/Controllers/Panel/User/UserController.php
@@ -5,14 +5,38 @@ namespace App\Http\Controllers\Panel\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Panel\User\UserRequest;
 use App\Models\User;
+use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
-        return view('panel.users.index' , compact('users'));
+        if (\Request()->ajax()){
+            $users = User::latest()->get();
+            return  DataTables::of($users)
+                ->addColumn('action', function ($user) {
+                    return  '<a class="btn btn-sm bg-transparent d-inline"
+                   href="'.route('panel.users.edit',$user->id). '"><i class="fas fa-pen fa-15m text-success"></i></a>
+                  <a onclick="deleteItem(event ,   '.  "'" .route('panel.users.destroy' , $user->id). "'"  .'   )" class="btn btn-sm bg-transparent d-inline delete-confirm"><i
+                   class="fas fa-trash fa-15m text-danger"></i></a>' ;
+                })->addColumn('status', function ($user) {
+                    if(is_null($user->status))  {$cssClass=  'danger';} else {$cssClass =  'success';}
+                    if(is_null($user->status))  {$body=  'غیر فعال';} else {$body =  'فعال';}
+                    return "<span class=\"badge py-1 bg-$cssClass\"> $body </span>" ;
+                })->addColumn('expireDate', function ($user) {
+                    $expireCssStyle = time() > $user->expireDate ? 'color : red' : '' ;
+                    return '<span style=" ' .$expireCssStyle. ' ">
+                    '. date('d-m-Y',$user->expireDate) .' </span>' ;
+                })->addColumn('email_verified_at' , function ($user) {
+                    if(is_null($user->email_verified_at))  {$cssClass=  'danger';} else {$cssClass =  'success';}
+                    if(is_null($user->email_verified_at))  {$body=  'تایید نشده';} else {$body =  'تایید شده';}
+                   return  "<span class=\"badge py-1 bg-$cssClass\"> $body </span>" ;
+                })->rawColumns([ 'email_verified_at' , 'status' , 'action'])
+                ->make(true);
+        }
+        return view('panel.users.index' );
     }
+
 
     public function create()
     {
--- resources/views/panel/users/index.blade.php
+++ resources/views/panel/users/index.blade.php
@@ -1,5 +1,57 @@
 @extends('panel.layouts.master')
 @section('title')  کاربران   @endsection
+@section('css')
+<link href="{{ URL::asset('/assets/libs/datatables/datatables.min.css') }}" rel="stylesheet" type="text/css"/>
+@endsection
+
+@section('script')
+    <script src="{{ URL::asset('/assets/libs/datatables/datatables.min.js') }}"></script>
+    <script>
+        $(function () {
+            fillDataTable();
+            function fillDataTable() {
+                $('#tbl_users').DataTable({
+                    "language": {
+                        "paginate": {
+                            "previous": "‹" ,
+                            "next": "›"
+                        }
+                    } ,
+                    scrollY:  false,
+                    scrollX:  false,
+                    "paging": true,
+                    "filter": true,
+                    "info": false,
+                    "columnDefs": [
+                        { "width": "50px", "targets": 0 },
+                        { "width": "100px", "targets": 1 },
+                        { "width": "120px", "targets": 2 },
+                        { "width": "130px", "targets": 3 },
+                        { "width": "90px", "targets": 4 },
+                        { "width": "40px", "targets": 5 },
+                        { "width": "85px", "targets": 6 },
+                        { orderable: false, targets: 6 },
+                    ] ,
+                    ajax: "{{ route('panel.users.index') }}",
+                    columns: [
+                        {data: 'id', name: 'id'},
+                        {data: 'name', name: 'name'},
+                        {data: 'email', name: 'email'},
+                        {data: 'created_at', name: 'created_at'},
+                        {data: 'email_verified_at', name: 'email_verified_at'},
+                        {data: 'status', name: 'status'},
+                        {
+                            data: 'action',
+                            name: 'action',
+                            orderable: true,
+                            searchable: true
+                        }]
+                });
+            }
+        })
+
+    </script>
+@endsection
 @section('content')
     @component('panel.components.breadcrumb')
         @slot('title')  کاربران  @endslot
@@ -36,40 +88,40 @@
                             </tr>
                             </thead>
                             <tbody>
-                            @foreach($users as $user)
-                                <tr>
-                                    <td>{{ $user->id }}</td>
-                                    <td>{{ $user->name }}</td>
-                                    <td>{{ $user->email }}</td>
-                                    <td>{{ $user->created_at }}</td>
+{{--                            @foreach($users as $user)--}}
+{{--                                <tr>--}}
+{{--                                    <td>{{ $user->id }}</td>--}}
+{{--                                    <td>{{ $user->name }}</td>--}}
+{{--                                    <td>{{ $user->email }}</td>--}}
+{{--                                    <td>{{ $user->created_at }}</td>--}}
 
-                                    <td> <span  class="badge py-1 bg-@php if (is_null($user->email_verified_at)){ echo 'danger' ; }  else{ echo 'success' ; } @endphp">
-                                            @if(is_null($user->email_verified_at))
-                                                تایید نشده
-                                                @else
-                                                تایید شده
-                                                @endif
-                                        </span>
-                                    </td>
-                                    <td> <span  class="badge py-1 bg-{{ $user->statusCssClass }}">
-                                           {{  $user->statusName() }}
-                                        </span>
-                                    </td>
+{{--                                    <td> <span  class="badge py-1 bg-@php if (is_null($user->email_verified_at)){ echo 'danger' ; }  else{ echo 'success' ; } @endphp">--}}
+{{--                                            @if(is_null($user->email_verified_at))--}}
+{{--                                                تایید نشده--}}
+{{--                                                @else--}}
+{{--                                                تایید شده--}}
+{{--                                                @endif--}}
+{{--                                        </span>--}}
+{{--                                    </td>--}}
+{{--                                    <td> <span  class="badge py-1 bg-{{ $user->statusCssClass }}">--}}
+{{--                                           {{  $user->statusName() }}--}}
+{{--                                        </span>--}}
+{{--                                    </td>--}}
 {{--                                    <td>--}}
 {{--                                        <span--}}
 {{--                                            class="badge py-1 bg-{{ $user->statusCssClass }}">{{ $user->statusName() }}</span>--}}
 {{--                                    </td>--}}
-                                    <td>
-                                        <a class="btn btn-sm bg-transparent d-inline"
-                                          onclick="deleteItem(event , '{{ route('panel.users.destroy' , $user->id) }}')"><i
-                                                class="fas fa-pen fa-15m text-success"></i></a>
+{{--                                    <td>--}}
+{{--                                        <a class="btn btn-sm bg-transparent d-inline"--}}
+{{--                                          onclick="deleteItem(event , '{{ route('panel.users.destroy' , $user->id) }}')"><i--}}
+{{--                                                class="fas fa-pen fa-15m text-success"></i></a>--}}
 
-                                        <a  class="btn btn-sm bg-transparent d-inline delete-confirm"
-                                            onclick="deleteItem(event , '{{ route('panel.users.destroy' , $user->id) }}')"><i
-                                                class="fas fa-trash fa-15m text-danger"></i></a>
-                                    </td>
-                                </tr>
-                            @endforeach
+{{--                                        <a  class="btn btn-sm bg-transparent d-inline delete-confirm"--}}
+{{--                                            onclick="deleteItem(event , '{{ route('panel.users.destroy' , $user->id) }}')"><i--}}
+{{--                                                class="fas fa-trash fa-15m text-danger"></i></a>--}}
+{{--                                    </td>--}}
+{{--                                </tr>--}}
+{{--                            @endforeach--}}
                             </tbody>
                         </table>
                     </div>
