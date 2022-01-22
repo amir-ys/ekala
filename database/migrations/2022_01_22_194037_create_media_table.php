d8a09c19a69dadb60e8d3d6e522bcec122adcb15
amir-ys <amir.you74@gmail.com>
2022-01-22 23:16:12 +0330

    Media :  add model and migration

---
+++ database/migrations/2022_01_22_194037_create_media_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->json('files');
+            $table->string('type');
+            $table->foreignId('user_id');
+            $table->foreignId('product_id')->constrained('products');
+            $table->string('client_file_name')->nullable();
+            $table->foreignId('is_primary')->default(0)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
